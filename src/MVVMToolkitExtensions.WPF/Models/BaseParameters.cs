dcf5795be666fd331a1e8bc8e364c6966ab1f780
Brandon <bkw1491@gmail.com>
2023-09-21 13:47:08 +0100

    Add missing project files.

---
+++ src/MVVMToolkitExtensions.WPF/Models/BaseParameters.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using MVVMToolkitExtensions.WPF.Interfaces;
+
+namespace MVVMToolkitExtensions.WPF.Models;
+
+public class BaseParameters : IParameters, IEnumerable
+{
+    private readonly Dictionary<string, object> _parameters = new();
+
+    protected BaseParameters() { }
+
+    protected BaseParameters(IDictionary<string, object> initialValues)
+    {
+        foreach(var item in initialValues)
+        {
+            _parameters[item.Key] = item.Value;
+        }
+    }
+
+
+    public T Get<T>(string key) => _parameters.TryGetValue(key, out var value) ? (T)value : throw new KeyNotFoundException();
+    public void Add(string key, object value) => _parameters[key] = value;
+
+    public IEnumerator GetEnumerator() => _parameters.GetEnumerator();
+}
