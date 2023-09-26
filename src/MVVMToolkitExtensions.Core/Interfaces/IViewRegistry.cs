957789e84af43033d86e81c0bf77ae04da3cf8c8
Brandon <bkw1491@gmail.com>
2023-09-26 18:05:00 +0100

    Clean up.

--- src/MVVMToolkitExtensions.Core/Interfaces/IViewRegistry.cs
+++ src/MVVMToolkitExtensions.Core/Interfaces/IViewRegistry.cs
@@ -4,6 +4,4 @@ internal interface IViewRegistry
 {
     Type this[Type viewType] { get; set; }
     bool Contains(Type viewType);
-    IEnumerable<Type> Views();
-    IEnumerable<Type> ViewModels();
 }
