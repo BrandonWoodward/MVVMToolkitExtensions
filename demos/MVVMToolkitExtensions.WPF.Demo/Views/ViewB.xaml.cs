a86ba91562dfe1d3b24b96584673a6942aad7546
Brandon <bkw1491@gmail.com>
2023-09-26 10:08:37 +0100

    Untrack demos

--- demos/MVVMToolkitExtensions.WPF.Demo/Views/ViewB.xaml.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Controls;
-
-namespace AutoWireViewModel.Demos.WPF.Views; 
-
-public partial class ViewB : UserControl {
-    public ViewB() {
-        InitializeComponent();
-    }
-}
