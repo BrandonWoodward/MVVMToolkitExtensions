37203252f847f898aed981a6ae9b1f46e4fdb8f9
Asine Tan <asine.tam@163.com>
2023-10-23 19:50:02 +0800

    add wpf demo

---
+++ src/WPFDemo/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using MVVMToolkitExtensions.WPF.Extensions;
+using WPFDemo.ViewModels;
+using WPFDemo.Views;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly IHost host = Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration(a =>
+            {
+                a.SetBasePath(Environment.CurrentDirectory);
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddViewModelMapping<MainWindow, MainWindowViewModel>();
+            })
+            .Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            await host.StartAsync();
+            host.Services.WithMainWindow<MainWindow>();
+        }
+    }
+}
---
+++ src/WPFDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WPFDemo.ViewModels
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _title = "MVVMToolkitExtensions Guide demo";
+
+        [ObservableProperty]
+        private string _hello = "Hello World";
+    }
+}
